ebfac6d62a6bce2aa1282369f8021a9f8f737068
jamshaidkhalid <jamshaidkhalid32@gmail.com>
2023-02-27 22:06:44 +0500

    db connected

---
+++ server.js
@@ -0,0 +1,13 @@
+//conncet mysql
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected successfully ");
+});
+
