9625ad8e086f16c929ff1190d47e0d12771a201e
jamshaidkhalid <jamshaidkhalid32@gmail.com>
2023-02-28 15:55:40 +0500

    apis to GET and POST Questions in quizzes

--- server.js
+++ server.js
@@ -29,18 +29,52 @@ app.get("/quizzes", function (req, res) {
 
 // POST /quizzes: Creates a new quiz with the provided title and description in the request body.
 app.post("/quizzes", function (req, res) {
-    const title = req.body.title;
-    const description = req.body.description;
+  const title = req.body.title;
+  const description = req.body.description;
+
+  connection.query(
+    "INSERT INTO quizzes (title, description) VALUES (?, ?)",
+    [title, description],
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send(`Quiz with ID ${results.insertId} has been created.`);
+    }
+  );
+});
+
+
+//   GET /quizzes/:quizId/questions: Returns a list of questions for the quiz with the provided quizId parameter.
+app.get("/quizzes/:quizId/questions", function (req, res) {
+    const quizId = req.params.quizId;
   
     connection.query(
-      "INSERT INTO quizzes (title, description) VALUES (?, ?)",
-      [title, description],
+      "SELECT * FROM questions WHERE quiz_id = ?",
+      quizId,
       function (error, results, fields) {
         if (error) throw error;
-        res.send(`Quiz with ID ${results.insertId} has been created.`);
+        res.send(results);
       }
     );
   });
+  
+  // POST /quizzes/:quizId/questions: Creates a new question for the quiz with the provided quizId parameter, with the question text and is_mandatory flag provided in the request body.
+  app.post("/quizzes/:quizId/questions", function (req, res) {
+    const quizId = req.params.quizId;
+    const questionText = req.body.question_text;
+    const isMandatory = req.body.is_mandatory || false;
+  
+    connection.query(
+      "INSERT INTO questions (quiz_id, question_text, is_mandatory) VALUES (?, ?, ?)",
+      [quizId, questionText, isMandatory],
+      function (error, results, fields) {
+        if (error) throw error;
+        res.send(
+          `Question with ID ${results.insertId} has been created for Quiz with ID ${quizId}.`
+        );
+      }
+    );
+  });
+  
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
