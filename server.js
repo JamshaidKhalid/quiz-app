a343c2d9e93c6035b6d5d3758c1b7ce6bf3ecbe8
jamshaidkhalid <jamshaidkhalid32@gmail.com>
2023-02-28 15:52:49 +0500

    api to get quizzes

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const router = express.Router();
-const bodyParser = require('body-parser');
+var mysql = require("mysql");
+const bodyParser = require("body-parser");
 const port = 3000;
-const connection = require('./database/mysqlConnection');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-connection()
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "admin",
+  database: "quizapp",
 });
 
+conn = connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected successfully ");
+});
+
+//GET /quizzes: Returns a list of all quizzes in the database.
+app.get("/quizzes", function (req, res) {
+  connection.query("SELECT * FROM quizzes", function (error, results, fields) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
